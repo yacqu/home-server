464516b91699e0ee73d1cc1984a422ef068032f7
SakeenaDigital <123984458+SakeenaDigital@users.noreply.github.com>
2023-09-04 12:02:47 -0700

    added a text saver and a time stamp thing

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, url_for, flash, redirect,  jsonify
+import datetime
+import time
 from time import sleep
 import os
 
@@ -8,6 +10,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def text():
+    if request.method == 'POST':
+
+        phoneNumber = request.form['phoneNumber']
+        message = request.form['message']
+        textSendTime = request.form['textSendTime']
+        textString = phoneNumber + "--" + message + "--" + textSendTime
+        textList = [phoneNumber, message, textSendTime]
+        print(textString)
+        print(datetime.datetime.now()) # prints 2023-09-04 12:01:40.347519
+
+        with open('db/messageRequest.txt', 'a') as writer:
+            writer.write('\n')
+            writer.write('\n'.join(textList))
+        
+
+
+    return render_template('text.html')
+
+@app.route('/sendTextFromMac', methods=['GET', 'POST'])
+def sendTextFromMac():
     if request.method == 'POST':
         print(request.form['message'])
     return render_template('text.html')
--- toolbox/imessageSender.py
+++ toolbox/imessageSender.py
@@ -1,4 +1,6 @@
 from py_imessage import imessage
+import datetime
+import time
 from time import sleep
 
 phone = "4806486823"
@@ -11,3 +13,16 @@ sleep(5)
 resp = imessage.status(guid)
 
 print(f'Message was read at {resp.get("date_read")}')
+
+
+def textSender(number, message, timestamp):
+    print(datetime.fromtimestamp(time.time()))
+
+    if timestamp == datetime.datetime.now():
+        print('now')
+
+    guid = imessage.send(number, message)
+
+    print(f'Message was read at {resp.get("date_read")}')
+
+
