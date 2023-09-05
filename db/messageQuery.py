be1e46b3c7f822db5c44fedbe24c16e703af771e
SakeenaDigital <123984458+SakeenaDigital@users.noreply.github.com>
2023-09-04 19:52:02 -0700

    added read_messages_for_number

--- db/messageQuery.py
+++ db/messageQuery.py
@@ -116,8 +116,64 @@ def messageFilter(messages):
     
     print(uniqueTextThreads)
     
+def read_messages_for_number(self_number='Me', phone_number_to_query='Phone_Number', n=10, human_readable_date=True):
+    # Connect to the database and execute a query to join message and handle tables
+    db_location = "/Users/yacqubabdirahman/Library/Messages/chat.db"
+    conn = sqlite3.connect(db_location)
+    cursor = conn.cursor()
+    query = """
+    SELECT message.ROWID, message.date, message.text, message.attributedBody, handle.id, message.is_from_me, message.cache_roomnames
+    FROM message
+    LEFT JOIN handle ON message.handle_id = handle.ROWID
+    WHERE handle.id = ?  -- Filter messages for a specific phone number
+    ORDER BY message.date DESC LIMIT ?
+    """
+    results = cursor.execute(query, (phone_number_to_query, n)).fetchall()
+    
+    # Initialize an empty list for messages
+    messages = []
+    messagesNew = []
 
+    # Loop through each result row and unpack variables
+    for result in results:
+        rowid, date, text, attributed_body, handle_id, is_from_me, cache_roomname = result
 
+        # Use self_number or handle_id as phone_number depending on whether it's a self-message or not
+        phone_number = self_number if handle_id is None else handle_id
+
+        # Use text or attributed_body as body depending on whether it's a plain text or rich media message
+        if text is not None:
+            body = text
+        elif attributed_body is not None:
+            # Decode and extract relevant information from attributed_body
+            attributed_body = attributed_body.decode('utf-8', errors='replace')
+            # Additional logic to extract body content from attributed_body
+            # ...
+            body = attributed_body  # Set the body content
+            
+        # Convert date from Apple epoch time to standard format using datetime module if human_readable_date is True  
+        if human_readable_date:
+            date_string = '2001-01-01'
+            mod_date = datetime.datetime.strptime(date_string, '%Y-%m-%d')
+            unix_timestamp = int(mod_date.timestamp()) * 1000000000
+            new_date = int((date + unix_timestamp) / 1000000000)
+            date = datetime.datetime.fromtimestamp(new_date).strftime("%Y-%m-%d %H:%M:%S")
+
+        mapping = get_chat_mapping()  # Get chat mapping from database location
+
+        try:
+            mapped_name = mapping[cache_roomname]
+        except:
+            mapped_name = None
+
+        messages.append(
+            {"rowid": rowid, "date": date, "body": body, "phone_number": phone_number, "is_from_me": is_from_me,
+             "cache_roomname": cache_roomname, 'group_chat_name': mapped_name})
+
+        messagesNew.append({"phone_number": phone_number, "body": body, "is_from_me": is_from_me})
+
+    conn.close()
+    return messages, messagesNew
 
 # ask the user for the location of the database
 db_location = "/Users/yacqubabdirahman/Library/Messages/chat.db"
@@ -128,7 +184,8 @@ n = "10"
 [messages, messagesNew] = read_messages(n)
 #print_messages(messagesNew)
 
+[messagesNUM, messagesNewNUM] = read_messages_for_number("+17632272653")
+print_messages(messagesNewNUM)
 messageFilter(messagesNew)
 # Remove the 2 lines above after testing -- they are for testing only
 
-{'+17632272653': {'textsFromMe': ['What about hw and lectures?', 'can you sit through them?', 'well adhd is a spectrum, maybe yours isn’t too bad.', 'But hey its 2023 they giving adderall to newborns', 'Go get a script 🤩'], 'textsToMe': ['I can if it’s in person I can’t take online classes ']}, '+16515920585': {'textsFromMe': [], 'textsToMe': ['it’s brown and pretty, I think i’d like it off me better ', 'i’ll send you a pici dw ', 'Loved “Yes bae I did munch 🤩”', 'I had so much fun, now we’re gonna eat hehe']}}
