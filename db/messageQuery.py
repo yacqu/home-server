5533856524d07d9e9734ff9569526a26aa7f5022
SakeenaDigital <123984458+SakeenaDigital@users.noreply.github.com>
2023-09-04 19:42:21 -0700

    added a message filter

--- db/messageQuery.py
+++ db/messageQuery.py
@@ -5,7 +5,8 @@ import os
 import json
 
 
-def get_chat_mapping(db_location):
+def get_chat_mapping():
+    db_location = "/Users/yacqubabdirahman/Library/Messages/chat.db"
     conn = sqlite3.connect(db_location)
     cursor = conn.cursor()
 
@@ -18,8 +19,9 @@ def get_chat_mapping(db_location):
 
     return mapping
 # Function to read messages from a sqlite database
-def read_messages(db_location, n, self_number='Me', human_readable_date=True):
+def read_messages(n, self_number='Me', human_readable_date=True):
     # Connect to the database and execute a query to join message and handle tables
+    db_location = "/Users/yacqubabdirahman/Library/Messages/chat.db"
     conn = sqlite3.connect(db_location)
     cursor = conn.cursor()
     query = """
@@ -69,7 +71,7 @@ def read_messages(db_location, n, self_number='Me', human_readable_date=True):
             new_date = int((date+unix_timestamp)/1000000000)
             date = datetime.datetime.fromtimestamp(new_date).strftime("%Y-%m-%d %H:%M:%S")
 
-        mapping = get_chat_mapping(db_location)  # Get chat mapping from database location
+        mapping = get_chat_mapping()  # Get chat mapping from database location
 
         try:
             mapped_name = mapping[cache_roomname]
@@ -80,14 +82,41 @@ def read_messages(db_location, n, self_number='Me', human_readable_date=True):
             {"rowid": rowid, "date": date, "body": body, "phone_number": phone_number, "is_from_me": is_from_me,
              "cache_roomname": cache_roomname, 'group_chat_name' : mapped_name})
         
-        messagesNew.append({"phone_number": phone_number, "body": body,})
+        messagesNew.append({"phone_number": phone_number, "body": body, "is_from_me": is_from_me})
 
     conn.close()
     return messages, messagesNew
 
 
 def print_messages(messages):
+
     print(json.dumps(messages))
+
+def messageFilter(messages):
+    uniqueTextThreads = {}
+    numbers = set([message['phone_number'] for message in messages])
+    uniqueNumbers = (list(numbers))
+
+    for number in uniqueNumbers:
+        uniqueTextThreads[number] = {"textsFromMe": [],"textsToMe": []}
+    
+    messages.reverse()
+    for message in messages:
+        if message['is_from_me'] == 1:
+            #print("Yacqub: ", message['body'])
+            for number in uniqueNumbers:
+                if number == message['phone_number']:
+                    uniqueTextThreads[number]["textsFromMe"].append(message['body'])
+
+        elif message['is_from_me'] == 0:
+            #print(message['phone_number'], ": ", message['body'])
+            for number in uniqueNumbers:
+                if number == message['phone_number']:
+                    uniqueTextThreads[number]["textsToMe"].append(message['body'])
+    
+    print(uniqueTextThreads)
+    
+
 
 
 # ask the user for the location of the database
@@ -96,6 +125,10 @@ db_location = "/Users/yacqubabdirahman/Library/Messages/chat.db"
 n = "10"
 
 # Remove the 2 lines below after testing -- they are for testing only
-[messages, messagesNew] = read_messages(db_location, n)
-print_messages(messagesNew)
+[messages, messagesNew] = read_messages(n)
+#print_messages(messagesNew)
+
+messageFilter(messagesNew)
 # Remove the 2 lines above after testing -- they are for testing only
+
+{'+17632272653': {'textsFromMe': ['What about hw and lectures?', 'can you sit through them?', 'well adhd is a spectrum, maybe yours isn’t too bad.', 'But hey its 2023 they giving adderall to newborns', 'Go get a script 🤩'], 'textsToMe': ['I can if it’s in person I can’t take online classes ']}, '+16515920585': {'textsFromMe': [], 'textsToMe': ['it’s brown and pretty, I think i’d like it off me better ', 'i’ll send you a pici dw ', 'Loved “Yes bae I did munch 🤩”', 'I had so much fun, now we’re gonna eat hehe']}}
