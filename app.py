33c31f240bda77574d3247a3d6c137bd4c37f2c5
SakeenaDigital <123984458+SakeenaDigital@users.noreply.github.com>
2023-09-04 18:04:52 -0700

    changed macbook permissions

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def sendTextFromMac():
     return render_template('text.html')
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=8003, debug=True)
+   app.run(host='0.0.0.0', port=8000, debug=True)
